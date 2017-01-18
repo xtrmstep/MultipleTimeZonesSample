722c4f177b278cb384d3e5f9a3029b305beb0a40
Alexander Goida <alexander.goida@gmail.com>
2017-01-18 08:49:17 +0200

    Examples with DateTime

---
+++ MultipleTimeZonesSample.Console/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using c = System.Console;
+
+namespace MultipleTimeZonesSample.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var original = new DateTime(2017, 1, 14, 1, 30, 0, DateTimeKind.Utc);
+            c.WriteLine("User input:");
+            c.WriteLine("Input:\t\t{0}", original);
+            c.WriteLine("ISO 8601:\t{0:O}", original);
+            c.WriteLine("RFC 1123:\t{0:R}", original);
+            c.WriteLine("Sortable:\t{0:s}", original);
+            c.WriteLine("UTC sortable:\t{0:u}", original);
+            c.WriteLine("UTC full:\t{0:U}", original);
+
+            c.WriteLine();
+
+            var utc = original.ToUniversalTime();
+            c.WriteLine("As UTC:");
+            c.WriteLine("Input:\t\t{0}", utc);
+            c.WriteLine("ISO 8601:\t{0:O}", utc);
+            c.WriteLine("RFC 1123:\t{0:R}", utc);
+            c.WriteLine("Sortable:\t{0:s}", utc);
+            c.WriteLine("UTC sortable:\t{0:u}", utc);
+            c.WriteLine("UTC full:\t{0:U}", utc);
+
+            c.WriteLine();
+
+            var local = original.ToLocalTime();
+            c.WriteLine("As Local:");
+            c.WriteLine("Input:\t\t{0}", local);
+            c.WriteLine("ISO 8601:\t{0:O}", local);
+            c.WriteLine("RFC 1123:\t{0:R}", local);
+            c.WriteLine("Sortable:\t{0:s}", local);
+            c.WriteLine("UTC sortable:\t{0:u}", local);
+            c.WriteLine("UTC full:\t{0:U}", local);
+
+
+
+            //var userInput1 = new DateTime(2017, 1, 14, 1, 30, 0, DateTimeKind.Utc);
+            //var utcTime1 = userInput.ToUniversalTime();
+
+            //System.Console.WriteLine(userInput.ToString("u"));
+            //System.Console.WriteLine(utcTime.ToString("u"));
+            //System.Console.WriteLine(userInput1.ToString("u"));
+            //System.Console.WriteLine(utcTime1.ToString("u"));
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ MultipleTimeZonesSample/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MultipleTimeZonesSample
+{
+    public class UtcTests
+    {
+        [Fact]
+        public void Should_return_all_timezones()
+        {
+            var tz = TimeZoneInfo.GetSystemTimeZones();
+            var sb = new StringBuilder();
+            sb.AppendLine("Id;BaseUtcOffset;DaylightName;DisplayName;StandardName;SupportsDaylightSavingTime");
+            foreach (var zoneInfo in tz)
+            {
+                sb.AppendLine(string.Format("{0};{1};{2};{3};{4};{5}",
+                    zoneInfo.Id, 
+                    zoneInfo.BaseUtcOffset, 
+                    zoneInfo.DaylightName,
+                    zoneInfo.DisplayName,
+                    zoneInfo.StandardName,
+                    zoneInfo.SupportsDaylightSavingTime));
+            }
+            var tzList = sb.ToString();
+            // set a breakpoint here and take the value of the string
+        }
+
+        [Fact]
+        public void Timezone_information_is_required_to_get_local_time()
+        {
+            var newYorkUtcTime = new DateTime(2017, 1, 14, 6, 30, 0);
+            var newYorkTimezone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            var newYorkLocalTime = TimeZoneInfo.ConvertTimeFromUtc(newYorkUtcTime, newYorkTimezone);
+            
+            Assert.Equal(new DateTime(2017, 1, 14, 1, 30, 0), newYorkLocalTime);
+        }
+
+        [Fact]
+        public void Should_see_correct_local_time_from_different_timezone_2()
+        {
+            var losAngelesUtcTime = new DateTime(2017, 1, 14, 9, 30, 0);
+
+            var losAngelesTimezone = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+            var losAngelesLocalTime = TimeZoneInfo.ConvertTimeFromUtc(losAngelesUtcTime, losAngelesTimezone);
+
+            Assert.Equal(new DateTime(2017, 1, 14, 1, 30, 0), losAngelesLocalTime);
+        }
+    }
+}
